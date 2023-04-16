d59bd266a628fd49ac08fa92764fe9a669bfb4bc
joaoagcosta <joaoagcosta@hotmail.com>
2023-04-16 18:39:37 +0100

    js, css and html

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message;
+    }
+    return {
+        renderMessage,
+    }
+})();
+
+
+
+
 const Gameboard = (() =>{
     let gameboard = ["", "", "", "", "", "", "", "", ""]
 
@@ -20,11 +32,12 @@ const Gameboard = (() =>{
         render();
     };
 
-    const getGameBoard = () => gameboard;
+    const getGameboard = () => gameboard;
     
     return{
         render,
         update,
+        getGameboard,
     }
 
 })();
@@ -58,23 +71,74 @@ const Game = (() =>{
     }
 
     const handleClick = (event) =>{
-        let index = parseInt(event.target.id.split("-")[1]);
-        
-
-        if(Gameboard.getGameBoard()[index] !== "")
-            currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+        if (gameOver){
             return;
-        Gameboard.update(index, players[currentPlayerIndex].mark)
+        }
+        let index = parseInt(event.target.id.split("-")[1]);
+        if(Gameboard.getGameboard()[index] !== "")
+            return;
+        
+        Gameboard.update(index, players[currentPlayerIndex].mark); 
+        
+        if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)){
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins`);
+        } else if (checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+            displayController.renderMessage("It's tie");
 
+        }
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+
+    }
+
+    const restart = () => {
+        for(let i = 0; i<9; i++ ){
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
+        gameOver = false;
+        document.querySelector("#message").innerHTML = "";
     }
 
 
     return{
         start,
+        restart,
         handleClick,
     }
 })();
+
+function checkForWin(board){
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for(let i = 0; i<winningCombinations.length; i++){
+        const [a,b,c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true;
+
+        }
+    }
+    return false;
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell != "")
+
+}
+
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+    Game.restart();
+})
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", ()=> {
